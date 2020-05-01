eda38f99e5f3e92301d734b9c0afb16c12cd3a21
nnamdi16 <nwabuokeinnamdi@gmail.com>
2020-05-01 22:44:26 +0100

    Created an EmployeeManager interface and EmployeeManagerImpl implementation class using @Service annotation injecting employeeDAO using field injection.

---
+++ src/main/java/spring/nnamdi/service/EmployeeManager.java
@@ -0,0 +1,9 @@
+package spring.nnamdi.service;
+
+import spring.nnamdi.model.EmployeeVO;
+
+import java.util.List;
+
+public interface EmployeeManager {
+    public List<EmployeeVO> getAllEmployees();
+}
---
+++ src/main/java/spring/nnamdi/service/EmployeeManagerImpl.java
@@ -0,0 +1,24 @@
+package spring.nnamdi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import spring.nnamdi.DAO.EmployeeDAO;
+import spring.nnamdi.model.EmployeeVO;
+
+import java.util.List;
+
+@Service
+public class EmployeeManagerImpl implements EmployeeManager {
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+//    @Autowired
+//    public void setEmployeeDAO(EmployeeDAO employeeDAO) {
+//        this.employeeDAO = employeeDAO;
+//    }
+
+    @Override
+    public List<EmployeeVO> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+}
